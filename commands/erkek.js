76ce3e853d42aa6128bbb944b35f43b0ce6bb4a4
Khont Polisian <82388102+KhontWasHere@users.noreply.github.com>
2022-01-22 12:07:35 +0200

    Update erkek.js

--- commands/erkek.js
+++ commands/erkek.js
@@ -19,24 +19,6 @@ const Discord = require("discord.js")
 const db = require("quick.db")
 const ayarlar = require("../ayarlar.json")
 
-module.exports.run = async (client, message, args) => {
-    if(!ayarlar.yetkiliRol.some(arwww => message.member.roles.cache.has(arwww)) && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${ayarlar.no} **Bu işlemi gerçekleştirmek için gerekli yetkin yok!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
-  
-    const etiketlenen = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-if(!etiketlenen) return message.channel.send(`${ayarlar.noemoji} **Kaydetmek için bir kişi etiketlemelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.noemoji)))
-  
-const isim = args[1];
-const yaş = args[2];
-if(!isim) return message.channel.send(`${ayarlar.noemoji} **Kaydetmek için bir isim belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.noemoji)))
-if(!yaş) return message.channel.send(`${ayarlar.noemoji} **Kaydetmek için bir yaş belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.noemoji)))
-if(isNaN(yaş)) return message.channel.send(`${ayarlar.noemoji} **Belirttiğin yaş rakamlardan oluşmalı!**`).then(message.react(client.emojis.cache.get(ayarlar.noemoji)))
-  
-etiketlenen.roles.add(ayarlar.erkek)
-etiketlenen.roles.remove(ayarlar.kayitsızRol)
-etiketlenen.setNickname(`${ayarlar.tag} ${isim} ${ayarlar.sembol} ${yaş}`)
-
-
-
 const arwEmbed = new Discord.MessageEmbed()
 .setColor("RANDOM")
 .setDescription(`Kullanıcının ismi \`${ayarlar.tag} ${isim} ${ayarlar.sembol} ${yaş}\` olarak değiştirildi       
@@ -56,7 +38,7 @@ Yetkili: message.author.id
 db.add(`erkekTeyit.${message.member.id}`, `1`)
 db.add(`toplamTeyit.${message.member.id}`, `1`)
 
-client.channels.cache.get(ayarlar.chat).send(` ${etiketlenen} **kaydolarak sunucuya giriş yaptı hoşgeldin.** <#921771656500899921> **Kanalından Rollerini Alabilirsin!**`)
+client.channels.cache.get(ayarlar.sohbetKanal).send(` ${etiketlenen} **kaydolarak sunucuya giriş yaptı hoşgeldin.** <#921771656500899921> **Kanalından Rollerini Alabilirsin!**`)
   
 }
 exports.config = {
